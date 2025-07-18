637617d461cf2522859a986b96afbe000e01666e
Oleg-Malyutin <65244598+Oleg-Malyutin@users.noreply.github.com>
2025-07-19 02:44:37 +0300

    Add files via upload

--- source/ieee802_15_4/tx_ieee802_15_4.cpp
+++ source/ieee802_15_4/tx_ieee802_15_4.cpp
@@ -34,49 +34,76 @@ void tx_ieee802_15_4::symbol_generate()
         }
     }
     for(int i = 0; i < LEN_MAX_SAMPLES * 2; ++i){
-        tx_data[i] = 0;
+        data[i] = 0;
     }
     offset_shr = 1;
     for(int c = 16 * 4 - 2; c < 16 * 4; ++c){
-        tx_data[offset_shr++] = chip_q[0][c];
+        data[offset_shr++] = chip_q[0][c];
         offset_shr++;
     }
     offset_shr = 0;
     for(int z = 0; z < 8; ++z){
         for(int c = 0; c < 16 * 4; ++c){
-            tx_data[offset_shr++] = chip_i[0][c];
-            tx_data[offset_shr + SHIFT_IQ] = chip_q[0][c];
+            data[offset_shr++] = chip_i[0][c];
+            data[offset_shr + SHIFT_IQ] = chip_q[0][c];
             ++offset_shr;
         }
     }
     for(int c = 0; c < 16 * 4; ++c){
-        tx_data[offset_shr++] = chip_i[0x7][c];
-        tx_data[offset_shr + SHIFT_IQ] = chip_q[0x7][c];
+        data[offset_shr++] = chip_i[0x7][c];
+        data[offset_shr + SHIFT_IQ] = chip_q[0x7][c];
         ++offset_shr;
     }
 //    tx_data[offset_shr - 2] = 2;
     for(int c = 0; c < 16 * 4; ++c){
-        tx_data[offset_shr++] = chip_i[0xa][c];
-        tx_data[offset_shr + SHIFT_IQ] = chip_q[0xa][c];
+        data[offset_shr++] = chip_i[0xa][c];
+        data[offset_shr + SHIFT_IQ] = chip_q[0xa][c];
         ++offset_shr;
     }
 }
 //--------------------------------------------------------------------------------------------------
-void tx_ieee802_15_4::start(tx_thread_data_t *tx_thread_data_)
+void tx_ieee802_15_4::start(tx_thread_data_t *tx_thread_data_, tx_thread_send_data *tx_thread_send_data_)
 {
-    return;
-
-
     symbol_generate();
 
-    callback->plot_test_callback(offset_shr + SHIFT_IQ, tx_data);
+    work(tx_thread_data_, tx_thread_send_data_);
+}
+//--------------------------------------------------------------------------------------------------
+void tx_ieee802_15_4::work(tx_thread_data_t *tx_thread_data_, tx_thread_send_data *tx_thread_send_data_)
+{
+    std::unique_lock<std::mutex> read_lock(tx_thread_send_data_->mutex);
+    while (!tx_thread_send_data_->stop){
+        tx_thread_send_data_->cond_value.wait(read_lock);
+        if(tx_thread_send_data_->ready){
+            tx_thread_send_data_->ready = false;
+            tx_data(tx_thread_data_, tx_thread_send_data_->buffer);
+        }
+    }
+    fprintf(stderr, "tx_ieee802_15_4::work() finish\n");
+}
+//--------------------------------------------------------------------------------------------------
+void tx_ieee802_15_4::tx_data(tx_thread_data_t *tx_thread_data_, std::vector<char> &buffer_)
+{
+    int len_data = buffer_.size();
+    int offset = offset_shr;
+    for(int s = 0; s < len_data; ++s){
+        int d = buffer_[s];
+        for(int c = 0; c < 16 * 4; ++c){
+            data[offset++] = chip_i[d][c];
+            data[offset + SHIFT_IQ] = chip_q[d][c];
+            ++offset;
+        }
+    }
 
     tx_thread_data_->mutex.lock();
-    tx_thread_data_->len_buffer = offset_shr + SHIFT_IQ;
-    tx_thread_data_->ptr_buffer = tx_data;
+    tx_thread_data_->len_buffer = offset + SHIFT_IQ;
+    tx_thread_data_->ptr_buffer = data;
     tx_thread_data_->ready = true;
     tx_thread_data_->mutex.unlock();
     tx_thread_data_->cond_value.notify_one();
+
+    int show_data = len_data == 0 ? 0 : offset_shr;
+    callback->plot_test_callback(offset + SHIFT_IQ - show_data, data + show_data);
 
     fprintf(stderr, "tx_ieee802_15_4::start= %d %d\n", LEN_MAX_SAMPLES * 2, offset_shr);
 }
